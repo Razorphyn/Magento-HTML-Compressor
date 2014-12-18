301f8f1301983b7503f92f70c344faec7733ac72
Luca Grandi <est.grandi@gmail.com>
2014-12-18 18:20:46 +0100

    Regex Improvements

--- lib/Razorphyn/html_compressor.php
+++ lib/Razorphyn/html_compressor.php
@@ -16,7 +16,7 @@
 		
 		$search=array(	
 						'@<(\s)*pre[^>]*>(?:[^<]+|)</pre>@',	//Find PRE Tag
-						'@<(\s)*textarea(\b[^>]*?>[\\s\\S]*?</textarea>)\s*@'	//Find TEXTAREA
+						'@<(\s)*textarea(\b[^>]*?>[\\s\\S]*?</textarea>)\s*@',	//Find TEXTAREA
 					);
 		$string=preg_replace_callback($search,
 										function($m){
@@ -32,17 +32,17 @@
 		$search = array(
 						'@( |\t|\f)+@',	// Shorten multiple whitespace sequences
 						'@^\s+|\s+$@',	// Trim lines
-						"/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/" //Remove empty lines
+						'@(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+@'	//Remove blank lines
 						);
-		$replace = array(' ','',"\n");
+		$replace = array(' ','',' ');
 
 		$string = preg_replace($search, $replace, $string);
 		
 		$search=array(	
 						'@<!--\[if\s(?:[^<]+|<(?!!\[endif\]-->))*<!\[endif\]-->@',	//Find IE Comments
-						'@<(\s*?)script(\b[^>]*?)>([\s\S]*?)</script>(\s*)@',	//Find SCRIPT Tag
+						'@^(<)\s*?(script\b[^>]*?)(>)([\s\S]*?)(<)\s*(/\s*?script\s*)(>)$@',	//Find SCRIPT Tag
 						'@//<!\[CDATA\[(?:[^<]+|)//]]>@',	//Find CDATA
-						'@<(\s)*style(\b[^>]*>)([\s\S]*?)</style>\s*@'	//Find STYLE Tag
+						'@^(<)\s*?(style\b[^>]*?)(>)([\s\S]*?)(<)\s*(/\s*?style\s*)(>)$@'	//Find STYLE Tag
 					);
 		$string=preg_replace_callback($search,
 										function($m){
@@ -56,7 +56,7 @@
 
 		$search = array(
 						'@(class|id|value|alt|href|src|style|title)=(\'\s*?\'|"\s*?")@',	//Remove empty attribute
-						'@<!--([\s\S]*?)-->@',	// Strip comments excluded IE
+						'@<!--([\s\S]*?)-->@',	// Strip comments except IE
 						'@(\r\n|\n|\r)@', // Strip break line
 						'@( |\t|\f)+@',	// Shorten multiple whitespace sequences
 						'@^\s+|\s+$@'	// Trim lines
@@ -65,13 +65,13 @@
 		$string = preg_replace($search, $replace, $string);
 
 		//Remove blank lines
-		$string=preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/"," ", $string);
+		$string=preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/",' ', $string);
 		
 		
 		//Replace unique id with script, style, pre original tag
 		$c=count($idarray);
 		for($i=0;$i<$c;$i++){
-			$string = str_replace($idarray[$i][0], $idarray[$i][1], $string);
+			$string = str_replace($idarray[$i][0], "\n".$idarray[$i][1]."\n", $string);
 		}
 
 		return $string;
