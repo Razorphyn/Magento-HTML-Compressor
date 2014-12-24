95cc2feacb08a247249eb81bb3f52a8ac5a7e58e
Luca Grandi <est.grandi@gmail.com>
2014-12-24 10:35:40 +0100

    Fix

--- lib/Razorphyn/HTMLCompressor/html_compressor.php
+++ lib/Razorphyn/HTMLCompressor/html_compressor.php
@@ -10,10 +10,10 @@
 
 	function html_compress($string){
 
-		//Substitute style, script, pre and cdata tag with unique id
 		global $idarray;
 		$idarray=array();
 		
+		//Replace PRE and TEXTAREA tags
 		$search=array(
 						'@(<)\s*?(pre\b[^>]*?)(>)([\s\S]*?)(<)\s*(/\s*?pre\s*?)(>)@',	//Find PRE Tag
 						'@(<)\s*?(textarea\b[^>]*?)(>)([\s\S]*?)(<)\s*?(/\s*?textarea\s*?)(>)@'	//Find TEXTAREA
@@ -28,23 +28,23 @@
 										$string
 		);
 		
+		//Remove blank useless space
 		$search = array(
 						'@( |\t|\f)+@',	// Shorten multiple whitespace sequences
-						'@^\s+|\s+$@',	// Trim lines
-						'@(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+@'	//Remove blank lines
+						'@(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+@',	//Remove blank lines
+						'@^(\s)+|( |\t|\0|\r\n)+$@' //Trim Lines
 						);
-		$replace = array(' ','',' ');
-
+		$replace = array(' ',"\\1",'');
 		$string = preg_replace($search, $replace, $string);
 
-		$search=array(	
+		//Replace IE COMMENTS, SCRIPT, STYLE and CDATA tags
+		$search=array(
 						'@<!--\[if\s(?:[^<]+|<(?!!\[endif\]-->))*<!\[endif\]-->@',	//Find IE Comments
 						'@(<)\s*?(script\b[^>]*?)(>)([\s\S]*?)(<)\s*?(/\s*?script\s*?)(>)@',	//Find SCRIPT Tag
 						'@(<)\s*?(style\b[^>]*?)(>)([\s\S]*?)(<)\s*?(/\s*?style\s*?)(>)@',	//Find STYLE Tag
 						'@(//<!\[CDATA\[([\s\S]*?)//]]>)@',	//Find commented CDATA
 						'@(<!\[CDATA\[([\s\S]*?)]]>)@'	//Find CDATA
 					);
-
 		$string=preg_replace_callback($search,
 										function($m){
 											$id='<!['.uniqid().']!>';
@@ -54,22 +54,20 @@
 										},
 										$string
 		);
-
+		
+		//Remove blank useless space
 		$search = array(
 						'@(class|id|value|alt|href|src|style|title)=(\'\s*?\'|"\s*?")@',	//Remove empty attribute
 						'@<!--([\s\S]*?)-->@',	// Strip comments except IE
-						'@(\r\n|\n|\r)@', // Strip break line
-						'@( |\t|\f)+@',	// Shorten multiple whitespace sequences
-						'@^\s+|\s+$@'	// Trim lines
+						'@[\r\n|\n|\r]@', // Strip break line
+						'@[ |\t|\f]+@',	// Shorten multiple whitespace sequences
+						'@(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+@', //Remove blank lines
+						'@^(\s)+|( |\t|\0|\r\n)+$@' //Trim Lines
 						);
-		$replace = array(' ','',' ',' ','');
+		$replace = array(' ','',' ',' ',"\\1",'');
 		$string = preg_replace($search, $replace, $string);
 
-		//Remove blank lines
-		$string=preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/",' ', $string);
-		
-		
-		//Replace unique id with script, style, pre original tag
+		//Replace unique id with original tag
 		$c=count($idarray);
 		for($i=0;$i<$c;$i++){
 			$string = str_replace($idarray[$i][0], "\n".$idarray[$i][1]."\n", $string);
